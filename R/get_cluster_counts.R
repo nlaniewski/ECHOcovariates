203c6d0958c39129a58630f365f6d8e38a84d40b
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2023-03-07 17:40:56 -0500

    data.table namespace

--- R/get_cluster_counts.R
+++ R/get_cluster_counts.R
@@ -3,11 +3,10 @@ get_cluster_counts<-function(cluster.counts.long.filepath,cluster.annotations.fi
   ##read in as a data.table; set factors
   cluster.counts <- data.table::fread(cluster.counts.long.filepath,stringsAsFactors = T)
   ##factor batch and cluster
-  cluster.counts[,data.table::`:=`(batch = factor(sprintf("%03d",batch)))]
-  cluster.counts[,data.table::`:=`(cluster = factor(cluster,levels=sort(unique(cluster))))]
+  cluster.counts[,batch := factor(sprintf("%03d",batch))]
+  cluster.counts[,cluster := factor(cluster,levels=sort(unique(cluster)))]
   ##relevel visit
-  cluster.counts[,data.table::`:=`(visit = factor(visit,levels=c(paste0("V",c(4,6,7)),"Adult"),
-                                      labels = c("Birth","6-months","12-months","Adult")))]
+  cluster.counts[,visit := factor(visit,levels=c(paste0("V",c(4,6,7)),"Adult"),labels = c("Birth","6-months","12-months","Adult"))]
   ##
   cell.types<-split(cluster.counts,by='cell.type')
   cell.types<-lapply(cell.types,function(i){
@@ -20,8 +19,8 @@ get_cluster_counts<-function(cluster.counts.long.filepath,cluster.annotations.fi
   ##add annotations
   if(!is.null(cluster.annotations.filepath)){
     cluster.annotations<-data.table::setDT(readxl::read_xlsx(cluster.annotations.filepath))
-    cluster.annotations[,data.table::`:=`(cluster = factor(cluster,levels=sort(unique(cluster))))]
-    cluster.annotations[,data.table::`:=`(cell.type = factor(cell.type,levels=as.character(levels(cluster.counts$cell.type))))]
+    cluster.annotations[,cluster := factor(cluster,levels=sort(unique(cluster)))]
+    cluster.annotations[,cell.type := factor(cell.type,levels=as.character(levels(cluster.counts$cell.type)))]
     ##merge
     cluster.counts<-data.table::merge.data.table(cluster.counts,cluster.annotations,by=c('cell.type','cluster'),all=T)
   }
